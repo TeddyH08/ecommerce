09402f8e5606c257c16bcae6f02a88840e72e9ea
TeddyH08 <teddy.hemonet@gmail.com>
2022-03-29 08:29:49 +0200

    nav

---
+++ assets/includes/navbar.php
@@ -0,0 +1,21 @@
+<nav>
+    <div class="logo">
+        <a href="index.php">Logo</a>
+    </div>
+
+    <div class="menu">
+        <ul>
+            <li>Nouveautés</li>
+            <li>Hommes</li>
+            <li>Femmes</li>
+            <li>Enfants</li>
+            <li>Sport</li>
+        </ul>
+    </div>
+
+    <div class="account">
+        <a href="">fav</a>
+        <a href="">panier</a>
+        <a href="">compte</a>
+    </div>
+</nav>
