0163603a031394b375508d828fd29a3f335265f7
TeddyH08 <teddy.hemonet@gmail.com>
2022-04-04 15:27:15 +0200

    up

--- assets/includes/ins.php
+++ assets/includes/ins.php
@@ -1,25 +1,36 @@
 <div class="container">
-    <div class="inscrption">
+    <div class="inscription">
         <h3>Inscription :</h3>
 
         <form action="">
-            <label for="" class="titr_la">Pseudo ou adresse mail :</label>
-            <input type="text" class="titr_in">
+            <div class="nom_prenom">
+                <div class="nom">
+                    <label for="">Nom</label>
+                    <input type="text">
+                </div>
 
-            <label for="" class="titr_la">Mot de passe :</label>
-            <input type="text" class="titr_in">
-
-            <div class="aide">
-                <div class="check"><input type="checkbox" class="checkbox"><label for="">Se souvenir de moi</label></div>
-                <a href="" class="mdp_oublie">mot de passe oublié ?</a>
+                <div class="prenom">
+                    <label for="">Prénom</label>
+                    <input type="text">
+                </div>
             </div>
 
-            <input type="submit" class="sub">
+            <div class="mail">
+                <label for="">Adresse mail :</label>
+                <input type="text">
+            </div>
+
+            <div class="mdp">
+                <label for="" class="titr_la">Mot de passe :</label>
+                <input type="text" class="titr_in">
+            </div>
+
+            <input type="submit" class="sub" value="S'inscrire">
         </form>
 
         <div class="co_ins">
-            <p>Pas enore membre ?</p>
-            <a href="inscription.php" class="mdp_oublie">Rejoignez-nous !</a>
+            <p>Déjà inscrit ?</p>
+            <a href="connexion.php" class="mdp_oublie">Connecte toi !</a>
         </div>
     </div>
 </div>
