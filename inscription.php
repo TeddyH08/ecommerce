365e734717d60132b521587bc6953cfd323d627c
TeddyH08 <teddy.hemonet@gmail.com>
2022-04-04 14:48:05 +0200

    up

---
+++ inscription.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Connexion</title>
+
+        <link rel="stylesheet" href="assets/style/style.css">
+        <link 
+        rel="stylesheet" 
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" 
+        integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" 4
+        crossorigin="anonymous" 
+        referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    </head>
+
+    <body>
+        <?php include "assets/includes/navbar.php"; ?>
+        <?php include "assets/includes/ins.php"; ?>
+        <?php include "assets/includes/footer.php"; ?>
+    </body>
+</html>
