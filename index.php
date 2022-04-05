b1086c0129a90a90d900115a6f867454521b3ed9
TeddyH08 <teddy.hemonet@gmail.com>
2022-04-05 09:27:43 +0200

    up

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="shortcut icon" href="assets/img/icon/icon.png">
+
         <title>E-Commerce</title>
 
         <link rel="stylesheet" href="assets/style/style.css">
