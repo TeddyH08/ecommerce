649f72b120a604ce445a841a8d157af4288300cc
TeddyH08 <teddy.hemonet@gmail.com>
2022-04-14 10:17:15 +0200

    up

--- assets/includes/fav.php
+++ assets/includes/fav.php
@@ -1,3 +1,21 @@
 <div class="container">
+    <div class="favo">
+        <h2 class="titre_h2">Mes favoris :</h2>
 
+        <div class="vide">
+            <p>Vous ne possédez pas encore de favoris.</p>
+        </div>
+
+        <div class="article_fav">
+            <div class="img_fav"><img src="assets/img/hauts/thumb_c290x3906af62e3a15061099ff52f933aa9857ba.jpg" class="fav_img"></div>
+        </div>
+
+        <div class="article_fav">
+            <div class="img_fav"><img src="assets/img/hauts/thumb_c290x3906af62e3a15061099ff52f933aa9857ba.jpg" class="fav_img"></div>
+        </div>
+        
+        <div class="article_fav">
+            <div class="img_fav"><img src="assets/img/hauts/thumb_c290x3906af62e3a15061099ff52f933aa9857ba.jpg" class="fav_img"></div>
+        </div>
+    </div>
 </div>
