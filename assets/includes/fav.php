0600b626d8fcc076ae737c8ef4f7c4bd2bd6f3e8
TeddyH08 <teddy.hemonet@gmail.com>
2022-04-14 13:28:37 +0200

    up fav

--- assets/includes/fav.php
+++ assets/includes/fav.php
@@ -2,20 +2,56 @@
     <div class="favo">
         <h2 class="titre_h2">Mes favoris :</h2>
 
-        <div class="vide">
+        <!-- <div class="vide">
             <p>Vous ne possédez pas encore de favoris.</p>
+        </div> -->
+
+        <div class="article_fav">
+            <div class="img_fav">
+                <img src="assets/img/hauts/thumb_c290x3906af62e3a15061099ff52f933aa9857ba.jpg" class="fav_img">
+            </div>
+
+            <div class="sous_img_fav">
+                <div>
+                    <img src="assets/img/hauts/thumb_c290x3900e1b7bcaec2cd8933ffec83e10c7d997.jpg" class="fav_img">
+                </div>
+
+                <div>
+                    <img src="assets/img/hauts/thumb_c290x39040e9b4a6b5c4fceb814c0ef728c79f99.jpg" class="fav_img">
+                </div>
+            </div>
         </div>
 
         <div class="article_fav">
-            <div class="img_fav"><img src="assets/img/hauts/thumb_c290x3906af62e3a15061099ff52f933aa9857ba.jpg" class="fav_img"></div>
-        </div>
+            <div class="img_fav">
+                <img src="assets/img/hauts/thumb_c290x3906af62e3a15061099ff52f933aa9857ba.jpg" class="fav_img">
+            </div>
 
-        <div class="article_fav">
-            <div class="img_fav"><img src="assets/img/hauts/thumb_c290x3906af62e3a15061099ff52f933aa9857ba.jpg" class="fav_img"></div>
+            <div class="sous_img_fav">
+                <div>
+                    <img src="assets/img/hauts/thumb_c290x3900e1b7bcaec2cd8933ffec83e10c7d997.jpg" class="fav_img">
+                </div>
+
+                <div>
+                    <img src="assets/img/hauts/thumb_c290x39040e9b4a6b5c4fceb814c0ef728c79f99.jpg" class="fav_img">
+                </div>
+            </div>
         </div>
         
         <div class="article_fav">
-            <div class="img_fav"><img src="assets/img/hauts/thumb_c290x3906af62e3a15061099ff52f933aa9857ba.jpg" class="fav_img"></div>
+            <div class="img_fav">
+                <img src="assets/img/hauts/thumb_c290x3906af62e3a15061099ff52f933aa9857ba.jpg" class="fav_img">
+            </div>
+
+            <div class="sous_img_fav">
+                <div>
+                    <img src="assets/img/hauts/thumb_c290x3900e1b7bcaec2cd8933ffec83e10c7d997.jpg" class="fav_img">
+                </div>
+
+                <div>
+                    <img src="assets/img/hauts/thumb_c290x39040e9b4a6b5c4fceb814c0ef728c79f99.jpg" class="fav_img">
+                </div>
+            </div>
         </div>
     </div>
 </div>
