c2047a386297e9635e90f0e6a50530089e3cbf94
TeddyH08 <teddy.hemonet@gmail.com>
2022-04-14 09:29:32 +0200

    first commit

--- assets/includes/navbar.php
+++ assets/includes/navbar.php
@@ -225,7 +225,7 @@
                     <input type="text" class="search-bar">
                     <a href="" class="search-button"><i class="fa-solid fa-magnifying-glass"></i></a>
                 </div>
-                <a href="" class="fav"><i class="fa-solid fa-heart"></i></a>
+                <a href="Favoris" class="fav"><i class="fa-solid fa-heart"></i></a>
                 <a href="" class="shop"><i class="fa-solid fa-bag-shopping"></i></a>
                 <a href="" class="acc"><i class="fa-solid fa-user"></i></a>
             </div>
