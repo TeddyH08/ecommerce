c9f4f2b60f8e0e12355a1001106d20beb76b9f24
Joannie3 <joannie1@outlook.fr>
2022-04-22 10:41:23 +0200

    up

--- assets/js/responsive.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.getElementById('toggle-button');
-const navList = document.getElementById('nav');
-
-toggleButton.addEventListener('click', () => {
-    navList.classList.toggle('active');
-    
-})
--- assets/js/tab.js
+++
@@ -1,4 +0,0 @@
-$(document).ready( function () {
-    $('#table_id').DataTable();
-    $('#table2_id').DataTable();
-} );
