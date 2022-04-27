58abaae896d65e667beede3b21d14278bb46cf5d
Teddy <96143755+TeddyH08@users.noreply.github.com>
2022-04-27 11:49:09 +0200

    Update connectdb.php

--- assets/db/connectdb.php
+++ assets/db/connectdb.php
@@ -1,9 +1,11 @@
 <?php
     try {
-        $db=new PDO('mysql:host=localhost;dbname=ecommerce','root','',[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-        $db->exec("SET NAMES utf8");
+        /* Mode Local */
+        //$db=new PDO('mysql:host=localhost;dbname=ecommerce','root','',[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        //$db->exec("SET NAMES utf8");
         
-        //$db = new PDO('mysql:host=localhost;dbname=e-commerce;charset=utf8', 'root', '',[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        /* Mode en ligne */
+        $db=new PDO('mysql:host=db5006773312.hosting-data.io;dbname=dbs5603904','dbu2407296','5Rc3y4Zg',[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
     } catch (Exception $e) {
         die('Erreur : ' . $e->getMessage());
     }
