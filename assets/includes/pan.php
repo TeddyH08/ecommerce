bf19189669f63a05c95df121b775966af9ef5686
yanis.pirlet <yanis.pirlet@gmail.com>
2022-04-29 10:54:32 +0200

    panier_dynamique

--- assets/includes/pan.php
+++ assets/includes/pan.php
@@ -3,11 +3,39 @@
        session_start();
         $id_login = $_SESSION['id'];
         $sqlajoutpanier = "SELECT * FROM panier WHERE id_utilisateurs = :id_utilisateurs";
-            $requeteajoutpanier = $db->prepare($sqlajoutpanier);
-            $requeteajoutpanier->execute(array(
-                ":id_utilisateurs" => $id_login 
-            ));    
-            $resultpaniers = $requeteajoutpanier->fetchAll(PDO::FETCH_ASSOC);
+        $requeteajoutpanier = $db->prepare($sqlajoutpanier);
+        $requeteajoutpanier->execute(array(
+            ":id_utilisateurs" => $id_login 
+        ));    
+        $resultpaniers = $requeteajoutpanier->fetchAll(PDO::FETCH_ASSOC);
+        
+        $sqlstock = "SELECT * FROM stocks";
+        $requetestock = $db->prepare($sqlstock);
+        $requetestock->execute();    
+        $resultstock = $requetestock->fetchAll(PDO::FETCH_ASSOC);
+        
+          
+        $sqlarticles = "SELECT * FROM articles";
+        $requetearticles = $db->prepare($sqlarticles);
+        $requetearticles->execute();    
+        $resultarticles = $requetearticles->fetchAll(PDO::FETCH_ASSOC);
+
+        $sqltailles = "SELECT * FROM tailles";
+        $requetetailles = $db->prepare($sqltailles);
+        $requetetailles->execute();    
+        $resulttailles = $requetetailles->fetchAll(PDO::FETCH_ASSOC);
+
+        
+        $sqlcat= "SELECT * FROM categories";
+        $requetecat = $db->prepare($sqlcat);
+        $requetecat->execute();    
+        $resultcat = $requetecat->fetchAll(PDO::FETCH_ASSOC);
+        
+        $id_paniers = "";
+            $query = "DELETE FROM panier WHERE panier . id_panier = :id_panier";
+              $retour = $db -> prepare($query);
+            $retour->execute(array(  ":id_panier" => $id_paniers ));
+
          ?> 
 <div class="container">
     <div class="pani">
@@ -17,15 +45,37 @@
                   
                 <div class="article_pan">
                     <div class="img_pan">
-                        <img src="assets/img/hauts/thumb_c290x3906af62e3a15061099ff52f933aa9857ba.jpg" class="pan_img">
-                    </div>
+                <?php    foreach ($resultarticles as $valuearticle) {
+                                    if ($valuedescription['id_articles'] == $valuearticle['id_articles']) {
+                                     foreach ($resultcat as $valuecat) {
+                                                                    if ($valuearticle['id_categories'] == $valuecat['id_categories']) {
+                                                                        echo "<img src='assets/img/". $valuecat['nom_categories'] ."/".  $valuearticle['image1_articles'] ." class='pan_img'>" ; 
+                                                                    }
+                                                                }
+                                     }} ?>    
+                                        </div>
+                    <?php
+                                foreach ($resultstock as $valuedescription) {
+                                    if ($value['id_stocks'] == $valuedescription['id_stocks']) {
+                                     ?>  
+                                      <div class="infos_pan">
+                                        <h4> <?php
+                                foreach ($resultarticles as $valuearticle) {
+                                    if ($valuedescription['id_articles'] == $valuearticle['id_articles']) {
+                                    echo $valuearticle['nom_articles']; }} ?> </h4>
+                                        <p>Référence : </p>
+                                        <p>Taille : <?php
+                                foreach ($resulttailles as $valuetailles) {
+                                    if ($valuedescription['id_tailles'] == $valuetailles['id_tailles']) {
+                                    echo $valuetailles['taille_tailles']; }} ?> </p>
+                                    <h4>Prix : <?php foreach ($resultarticles as $valuearticle) {
+                                    if ($valuedescription['id_articles'] == $valuearticle['id_articles']) {
+                                    echo $valuearticle['prix_articles']; }} ?> € </h4>
+                                </div>
+                                 <?php   }
+                                } ?>
                     
-                    <div class="infos_pan">
-                        <h4>Nom de l'article <?php  echo $value['id_panier'];?></h4>
-                        <p>Référence :</p>
-                        <p>Taille : <?php  echo $value['id_utilisateurs'];?></p>
-                    <h4>Prix :<?php  echo $value['id_stocks'];?> </h4>
-                </div>
+                   
                 
                 <div class="quantite_pan">
                     <h4>Quantité :</h4>
@@ -48,21 +98,19 @@
         
         <div class="resume">
             <h4>Résumé :</h4>
-
+            <?php foreach ($resultpaniers as $value) { ?>
             <div class="artic">
-                <p>Nom de l'article</p>
-                <p>Prix</p>
+           
+                <p> <?php
+                                foreach ($resultarticles as $valuearticle) {
+                                    if ($valuedescription['id_articles'] == $valuearticle['id_articles']) {
+                                    echo $valuearticle['nom_articles']; }} ?>
+                </p>
+                <p><?php foreach ($resultarticles as $valuearticle) {
+                                    if ($valuedescription['id_articles'] == $valuearticle['id_articles']) {
+                                    echo $valuearticle['prix_articles']; }} ?> €</p>
             </div>
-
-            <div class="artic">
-                <p>Nom de l'article</p>
-                <p>Prix</p>
-            </div>
-
-            <div class="artic">
-                <p>Nom de l'article</p>
-                <p>Prix</p>
-            </div>
+            <?php } ?> 
 
             <div class="sepa"></div>
 
